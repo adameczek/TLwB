7c68cc92c224506ac7c5f09a68e0507e302b064f
Adam Sawicki <adam2sawicki@gmail.com>
2021-01-17 19:05:58 +0100

    Add numbers.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchInverseException {
+	// write your code here
+        int x = 1, y = 1;
+        Numbers numbers = new Numbers();
+        //System.out.println("Euler= " + numbers.euler(7621));
+        System.out.println("Odwrotnosc modulo=" + numbers.mmi(631, 11));
+    }
+}
