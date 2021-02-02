97b79995aeae4419f4a9d36086bc34532b854c54
Vitaliy <sklyar.vitaliy@gmail.com>
2021-02-02 20:21:04 +0100

    change variable name

--- src/com/company/Eratosfen.java
+++ src/com/company/Eratosfen.java
@@ -3,14 +3,14 @@ package com.company;
 class Eratosfen {
     public static void main(String args[]) {
         int i, j;
-        boolean isprime;
+        boolean isPrime;
         for(i=1999900000; i < 2000099990; i++) {
-            isprime = true;
+            isPrime = true;
             // sprawdza czy liczba ma resztę po dzielieniu
             for (j=2; j <= i/j; j++)
                 // czy liczba jest liczbą pierwszą
-                if((i%j) == 0) isprime = false;
-            if (isprime)
+                if((i%j) == 0) isPrime = false;
+            if (isPrime)
                 System.out.println(i + " - Simple Number");
         }
     }
